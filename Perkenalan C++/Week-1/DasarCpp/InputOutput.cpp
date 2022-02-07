ca7f3d74b7db89d97aee44c00524356143f642a5
Felixarajiph <felixarajiph@gmail.com>
2022-02-07 16:44:52 +0700

    Create InputOutput.cpp

---
+++ Perkenalan C++/Week-1/DasarCpp/InputOutput.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+  char nama[20];//deklarasi variabel nma
+  int umur; //deklarasi variabel umur
+  cout<<"Masukan nama: "; //Output string
+  getline(cin,nama);   //mengambil input dalam satu line break
+  cout<<"Berapa umurmu?: "; 
+  cin>>umur; //mengambil imput
+  cout<<"Namamu adalah "<<nama<<"Dan umurmu adalah "<<umur; //output
+  }
