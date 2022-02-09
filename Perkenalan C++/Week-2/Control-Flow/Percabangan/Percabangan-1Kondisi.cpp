5c874d2b078691cd7dfd6956c96e452cf05c820f
Felixarajiph <felixarajiph@gmail.com>
2022-02-09 09:47:39 +0700

    Add files via upload

---
+++ Perkenalan C++/Week-2/Control-Flow/Percabangan/Percabangan-1Kondisi.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+  string nama;
+  int umur;
+  cout<<"If statement"<<endl;
+  cout<<"Masukan nama anda: ";
+  getline(cin,nama);
+  cout<<"Umur anda: ";
+  cin>>umur;
+  if(umur>=18){
+    cout<<"Kamu sudah cukup umur untuk mengendari sepeda motor";
+  }
+}
