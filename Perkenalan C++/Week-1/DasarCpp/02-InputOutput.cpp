30ae4abe063b99b667e42b11acb513207ff136bc
Sankage technology <99163441+SankageTech@users.noreply.github.com>
2022-02-11 19:50:14 +0700

    Update 02-InputOutput.cpp

--- Perkenalan C++/Week-1/DasarCpp/02-InputOutput.cpp
+++ Perkenalan C++/Week-1/DasarCpp/02-InputOutput.cpp
@@ -4,7 +4,7 @@ int main(){
   char nama[20];//deklarasi variabel nma
   int umur; //deklarasi variabel umur
   cout<<"Masukan nama: "; //Output string
-  getline(cin,nama);   //mengambil input dalam satu line break
+  cin.getline(nama,20);   //mengambil input dalam satu line break
   cout<<"Berapa umurmu?: "; 
   cin>>umur; //mengambil imput
   cout<<"Namamu adalah "<<nama<<"Dan umurmu adalah "<<umur; //output
