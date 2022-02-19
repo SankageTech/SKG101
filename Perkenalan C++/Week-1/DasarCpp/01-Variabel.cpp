81bc48f55d402f8411e404b4d55e6741d03f319a
Sankage technology <99163441+SankageTech@users.noreply.github.com>
2022-02-19 22:15:29 +0700

    Update 01-Variabel.cpp

--- Perkenalan C++/Week-1/DasarCpp/01-Variabel.cpp
+++ Perkenalan C++/Week-1/DasarCpp/01-Variabel.cpp
@@ -10,9 +10,10 @@ int main(){
   bool is_subscribe=false;
   //ini deklarasi variabel secara implicit
   cout<<"Masukan nama: ";
-  cin>>nama; //nama akan diganti oleh inputan
+  cin>>Nama; //nama akan terupdate oleh inputan
   cout<<"Umurmu: ";
-  cin>>a;
+  cin>>a; //value dari a akan terupdate
+  cout<<"Namamu adalah "<<Nama<<" umurmu adalah:"<<a;
   //variabel juga bisa di deklarasi seperti ini
   int z(12);
   int Haji{12};
